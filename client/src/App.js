445a4c8e895808df853adb95437f17c7bfdd69a4
Alvy <floressuarezalvaro@gmail.com>
2021-02-21 18:20:44 -0800

    readding app for successful backend message

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,10 @@ function App() {
       .catch((err) => console.log(err));
   });
   return (
-    <HelloDiv />
-    // <div className="App">
-    //   <h1>Hello World - FIrst REACT app</h1>
-    // </div>
+    // <HelloDiv />
+    <div className="App">
+      <h1>Hello World - FIrst REACT app</h1>
+    </div>
   );
 }
 export default App;
