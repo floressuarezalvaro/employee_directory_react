70dfb8ace50d31a806bb74f4190d3a697a55970c
Alvy <floressuarezalvaro@gmail.com>
2021-02-21 18:00:46 -0800

    updating components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import HelloDiv from "./components/HelloDiv";
 function App() {
+  useEffect(() => {
+    axios
+      .get("/example")
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelloDiv />
+    // <div className="App">
+    //   <h1>Hello World - FIrst REACT app</h1>
+    // </div>
   );
 }
-
 export default App;
