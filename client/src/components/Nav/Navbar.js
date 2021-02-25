d07c1667f436fff9408859336592f3e51834f643
Alvy <floressuarezalvaro@gmail.com>
2021-02-24 19:41:15 -0800

    added bootsrap navbar

--- client/src/components/Nav/Navbar.js
+++ client/src/components/Nav/Navbar.js
@@ -1,7 +1,49 @@
 import React from "react";
 
-function Navbar() {
-  return <h1>Hello NavBar</h1>;
-}
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Employee Directory
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+              <a className="nav-link" href="#">
+                Features
+              </a>
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+              <a
+                className="nav-link disabled"
+                href="#"
+                tabIndex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
