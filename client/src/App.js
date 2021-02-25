0cb36ab0b899489014dfe0e4947b8706e0067f0a
Alvy <floressuarezalvaro@gmail.com>
2021-02-24 19:23:08 -0800

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-// import HelloDiv from "./components/HelloDiv";
+import Navbar from "./components/Nav/Navbar";
+
 function App() {
   useEffect(() => {
     axios
       .get("/example")
       .then((response) => console.log(response))
       .catch((err) => console.log(err));
-  });
+  }, []);
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello World - FIrst REACT app</h1>
     </div>
   );
